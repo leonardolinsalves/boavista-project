cc5df103e8a17879fcf4609bbf5ef2a2ec7acd03
llinsalves <33530938+llinsalves@users.noreply.github.com>
2020-09-16 18:14:33 -0300

    Update copia_arq_bucket.py
    Remoção de sujeira comentada

--- code/copia_arq_bucket.py
+++ code/copia_arq_bucket.py
@@ -28,25 +28,3 @@ def upload_arquivo(bucket_name, source_file_name, destination_blob_name):
         f"{hora_processamento} Arquivo {source_file_name} carregado com sucesso em {destination_blob_name}"
         )
     return True
-
-
-
-'''
-def f_gsutil_copy_comp_boss():
-
-	os.system('gsutil cp /home/leonardo/Entrevista/TesteEngenheiroDados/data/comp_boss/comp_boss.csv gs://teste_boavista/source_data')
-	print(f'{hora_processamento} Arquivo comp_boss copiado com sucesso no Bucket')
-	return True
-
-def f_gsutil_copy_bill_materials():
-
-	os.system('gsutil cp /home/leonardo/Entrevista/TesteEngenheiroDados/data/bill_of_materials/bill_of_materials.csv gs://teste_boavista/source_data/')
-	print(f'{hora_processamento} Arquivo bill_of_materials copiado com sucesso no Bucket')
-	return True
-
-def f_gsutil_copy_price_quote():
-
-	os.system('gsutil cp /home/leonardo/Entrevista/TesteEngenheiroDados/data/price_quote/price_quote.csv gs://teste_boavista/source_data/')
-	print(f'{hora_processamento} Arquivo price_quote copiado com sucesso no Bucket')
-	return True
-	'''
